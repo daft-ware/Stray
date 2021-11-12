ced53cfa5f98d019f3f413b9ef61263f2b33ea65
scraptechguy <rostislav.bro@gmail.com>
2021-11-12 20:41:56 +0100

    Initial Commit

---
+++ Stray/StrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrayApp.swift
+//  Stray
+//
+//  Created by Rostislav Brož on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct StrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
