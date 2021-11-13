cbba70eb87376f2a375b050c9e3837559e7e586c
scraptechguy <rostislav.bro@gmail.com>
2021-11-13 11:20:52 +0100

    Create MenuView.swift

---
+++ Stray/Views/SheetViews/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  Stray
+//
+//  Created by Rostislav Brož on 11/13/21.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
