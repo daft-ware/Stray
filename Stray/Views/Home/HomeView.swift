09f33e5d46c72fd2d0818d2ed25c9e7e37c3a985
scraptechguy <rostislav.bro@gmail.com>
2021-11-13 11:31:30 +0100

    Create HomeView.swift

--- Stray/Views/Home/HomeView.swift
+++ Stray/Views/Home/HomeView.swift
@@ -7,11 +7,109 @@
 
 import SwiftUI
 
-struct HomeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct SearchPlaceholderStyle: ViewModifier {
+    var showPlaceHolder: Bool
+    var placeholder: String
+
+    public func body(content: Content) -> some View {
+        ZStack(alignment: .leading) {
+            if showPlaceHolder {
+                Text(placeholder)
+                    .padding(.horizontal, 15)
+                    .foregroundColor(darkmodeWhite)
+            }
+            content
+                .foregroundColor(darkmodeGray3)
+                .padding(5.0)
+        }
     }
 }
+
+
+struct HomeView: View {
+    
+    @State private var showingMenuSheet = false
+    
+    @State private var search: String = ""
+    @State private var isEditingSearch = false
+    
+    @State var classrooms: [String] = ["304", "80", "734", "320", "567", "230", "340"]
+    
+    
+    var body: some View {
+        ZStack {
+            darkmodeBlack1.ignoresSafeArea()
+    
+            VStack {
+                HStack {
+                    ZStack {
+                        Rectangle()
+                            .fill(darkmodeGray3)
+                            .frame(width: screenSize.width / 1.8, height: screenSize.width / 7)
+                            .cornerRadius(15)
+                        
+                        TextField("", text: $search)
+                            .modifier(SearchPlaceholderStyle(showPlaceHolder: search.isEmpty,
+                                placeholder: "Search your school"))
+                            .disableAutocorrection(true)
+                            .frame(width: screenSize.width / 2, height: screenSize.width / 7, alignment: .center)
+                    }
+                
+                    ZStack {
+                        Rectangle()
+                            .fill(darkmodeGray3)
+                            .frame(width: screenSize.width / 7, height: screenSize.width / 7)
+                            .cornerRadius(15)
+                        
+                        Button(action: {showingMenuSheet.toggle()}, label: {
+                            Image(systemName: "gear")
+                                .resizable()
+                                .foregroundColor(darkmodeWhite)
+                                .frame(width: screenSize.width / 13, height: screenSize.width / 13)
+                        }).sheet(isPresented: $showingMenuSheet) {
+                                MenuView()
+                            }
+                    }
+                    
+                    ZStack {
+                        Rectangle()
+                            .fill(darkmodeGray3)
+                            .frame(width: screenSize.width / 7, height: screenSize.width / 7)
+                            .cornerRadius(15)
+                        
+                        Button(action: {showingMenuSheet.toggle()}, label: {
+                            Image(systemName: "list.bullet")
+                                .resizable()
+                                .foregroundColor(darkmodeWhite)
+                                .frame(width: screenSize.width / 14, height: screenSize.width / 15)
+                        }).sheet(isPresented: $showingMenuSheet) {
+                                MenuView()
+                            }
+                    }
+                }
+                
+                HStack {
+                    ScrollView {
+                        
+                    }
+                }
+                
+                Spacer()
+                Spacer()
+                
+                Button(action: {showingMenuSheet.toggle()}, label: {
+                    Text("235\nChemistry with Ms. Roušalová")
+                        .foregroundColor(darkmodeWhite)
+                }).sheet(isPresented: $showingMenuSheet) {
+                        MenuView()
+                    }
+                
+            }
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
--- Stray/Views/LaunchView.swift
+++ Stray/Views/LaunchView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 
+// constant from which screen width and height is accessed
+// to properly position and size objects
+let screenSize: CGRect = UIScreen.main.bounds
+
+
+// declaration of the UI colors as variables so it's easy to change them
+// at any time
+
+public var darkmodeBlack1 = Color(red: 0.004, green: 0, blue: 0)
+public var darkmodeGray1 = Color(red: 0.6566, green: 0.6566, blue: 0.6566)
+public var darkmodeGray2 = Color(red: 0.2266, green: 0.2266, blue: 0.2266)
+public var darkmodeGray3 = Color(red: 0.1333, green: 0.1333, blue: 0.1373)
+public var darkmodeWhite = Color(red: 1, green: 1, blue: 1)
+
+
 struct LaunchView: View {
     var body: some View {
         Text("Done")
